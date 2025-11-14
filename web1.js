fe26a890c662106c54e148b28a406b8dd93abe77
zhuroushengjianbao <xutianzhe0609@gmail.com>
2025-11-15 01:14:48 +1300

    change "id" to "class"

--- web1.js
+++ web1.js
@@ -1,57 +1,55 @@
-function showHome(){
-    document.getElementById("home").style.display = "block";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("applicationCenter").style.display = "none";
-    document.getElementById("industryApplication").style.display = "none";
-    document.getElementById("newsEvents").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+function showHome() {
+    document.querySelector('.home').style.display = "block";
+    document.querySelector('.about').style.display = "none";
+    document.querySelector('.applicationCenter').style.display = "none";
+    document.querySelector('.industryApplication').style.display = "none";
+    document.querySelector('.newsEvents').style.display = "none";
+    document.querySelector('.contact').style.display = "none";
 }
 
 function showAbout(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "block";
-    document.getElementById("applicationCenter").style.display = "none";
-    document.getElementById("industryApplication").style.display = "none";
-    document.getElementById("newsEvents").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.querySelector('.home').style.display = "none";
+    document.querySelector('.about').style.display = "block";
+    document.querySelector('.applicationCenter').style.display = "none";
+    document.querySelector('.industryApplication').style.display = "none";
+    document.querySelector('.newsEvents').style.display = "none";
+    document.querySelector('.contact').style.display = "none";
 } 
 
 function showApplicationCenter(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("applicationCenter").style.display = "block";
-    document.getElementById("industryApplication").style.display = "none";
-    document.getElementById("newsEvents").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.querySelector('.home').style.display = "none";
+    document.querySelector('.about').style.display = "none";
+    document.querySelector('.applicationCenter').style.display = "block";
+    document.querySelector('.industryApplication').style.display = "none";
+    document.querySelector('.newsEvents').style.display = "none";
+    document.querySelector('.contact').style.display = "none";
 }
 
 function showIndustryApplication(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("applicationCenter").style.display = "none";
-    document.getElementById("industryApplication").style.display = "block";
-    document.getElementById("newsEvents").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.querySelector('.home').style.display = "none";
+    document.querySelector('.about').style.display = "none";
+    document.querySelector('.applicationCenter').style.display = "none";
+    document.querySelector('.industryApplication').style.display = "block";
+    document.querySelector('.newsEvents').style.display = "none";
+    document.querySelector('.contact').style.display = "none";
 }
 
 function showNewsEvents(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("applicationCenter").style.display = "none";
-    document.getElementById("industryApplication").style.display = "none";
-    document.getElementById("newsEvents").style.display = "block";
-    document.getElementById("contact").style.display = "none";
-
+    document.querySelector('.home').style.display = "none";
+    document.querySelector('.about').style.display = "none";
+    document.querySelector('.applicationCenter').style.display = "none";
+    document.querySelector('.industryApplication').style.display = "none";
+    document.querySelector('.newsEvents').style.display = "block";
+    document.querySelector('.contact').style.display = "none";
 }
 
 function showContact(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("applicationCenter").style.display = "none";
-    document.getElementById("industryApplication").style.display = "none";
-    document.getElementById("newsEvents").style.display = "none";
-    document.getElementById("contact").style.display = "block";
-
+    document.querySelector('.home').style.display = "none";
+    document.querySelector('.about').style.display = "none";
+    document.querySelector('.applicationCenter').style.display = "none";
+    document.querySelector('.industryApplication').style.display = "none";
+    document.querySelector('.newsEvents').style.display = "none";
+    document.querySelector('.contact').style.display = "block";
 }
 
 window.onload = function() {
